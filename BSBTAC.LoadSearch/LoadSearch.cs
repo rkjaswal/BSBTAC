964b885d4a535926a708f4c4d252f2c80bb177d3
Rajeev Jaswal <rkjaswal@gmail.com>
2014-09-15 22:29:55 +0100

    Used SqlBulkCopy
    Used SqlBulkCopy

--- BSBTAC.LoadSearch/LoadSearch.cs
+++ BSBTAC.LoadSearch/LoadSearch.cs
@@ -4,8 +4,11 @@ using Common.DAL;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Data.Entity.Core;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -33,12 +36,17 @@ namespace BSBTAC.LoadSearch
         protected override void OnStart(string[] args)
         {
             _log.Info("Load Search Service Started...");
+            Task loadUpload = new Task(() => LoadNewUploadsUsingBulk());
+            loadUpload.Start();
+            Thread.Sleep(60 * 1000);
+            /*
             while (true)
             {
                 Task loadUpload = new Task(() => LoadNewUploads());
                 loadUpload.Start();
                 Thread.Sleep(60 * 1000);
             }
+            */ 
         }
 
         protected override void OnStop()
@@ -134,5 +142,90 @@ namespace BSBTAC.LoadSearch
                 _log.Error(ex.Message);
             }
         }
+
+        private void LoadNewUploadsUsingBulk()
+        {
+            _uow = new UnitOfWork(bsbtacContext);
+            try
+            {
+                UploadDetail upload;
+                lock (this)
+                {
+                    upload = _uow.Repository<UploadDetail>().SqlQuery("spGetNextUploadedFile").First();
+                }
+
+                var filename = upload.Filename;
+
+                var path = @"C:\_Workspace\Projects\2012\BSBTAC\_Uploads\BSB";
+
+                /*
+                string strSql = "SELECT * FROM [" + filename + "]";
+                string strCSVConnString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + ";" + "Extended Properties='text;HDR=YES;FMT=Delimited(,)';";
+                
+                OleDbDataAdapter oleda = new OleDbDataAdapter(strSql, strCSVConnString); 
+                */
+
+                string connString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties='text;HDR=Yes;FMT=Delimited(,)';", path);
+                using (OleDbConnection con = 
+                        new OleDbConnection(connString))
+                {
+                    using (OleDbCommand cmd = new OleDbCommand(string.Format
+                                              ("SELECT * FROM [{0}]", filename), con))
+                    {
+                        con.Open();
+                        /* 
+                        // Using a DataReader to process the data
+                        using (OleDbDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Process the current reader entry...
+                            }
+                        }
+                        */
+                        using (OleDbDataAdapter adp = new OleDbDataAdapter(cmd))
+                        {
+                            DataTable tbl = new DataTable();
+                            adp.Fill(tbl);
+
+                            SqlBulkCopy bulkCopy = new SqlBulkCopy(ConfigurationManager.ConnectionStrings["BSBTACNonEF"].ConnectionString);
+                            bulkCopy.BatchSize = 1000;
+
+                            bulkCopy.ColumnMappings.Add(0, 2);
+                            bulkCopy.ColumnMappings.Add(1, 3);
+                            bulkCopy.ColumnMappings.Add(2, 4);
+                            bulkCopy.ColumnMappings.Add(3, 5);
+                            bulkCopy.ColumnMappings.Add(4, 6);
+                            bulkCopy.ColumnMappings.Add(5, 7);
+                            bulkCopy.ColumnMappings.Add(6, 8);
+                            bulkCopy.ColumnMappings.Add(7, 9);
+                            bulkCopy.ColumnMappings.Add(8, 10);
+                            bulkCopy.ColumnMappings.Add(9, 11);
+                            bulkCopy.ColumnMappings.Add(10, 12);
+                            bulkCopy.ColumnMappings.Add(11, 13);
+
+                            bulkCopy.DestinationTableName = "SearchDefinition";
+                            bulkCopy.WriteToServer(tbl);
+                            /*
+                            foreach (DataRow row in tbl.Rows)
+                            {
+                            }
+                            */
+                        }
+                    }
+                }
+
+
+            }
+            catch (EntityCommandExecutionException)
+            {
+                _log.Info("No new uploads found");
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+            }
+        }
+    
     }
 }
